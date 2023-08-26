a81430b2ad84495f71d01a37852fa346946468d2
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2023-08-26 17:51:45 +0530

    Corousel done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,35 +1,88 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 
-const Carousel = ({ images, controls }) => {
+const cards = [
+  {
+    index: 0,
+    heading: "Hello World",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat.",
+    image:
+      "https://assets.churchofjesuschrist.org/f8/17/f817e4a21336ad73433b4f1f0a5dfca77c6271d41049592/man_studying_reading.jpeg",
+  },
+  {
+    index: 1,
+    heading: "Hello World1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat.",
+    image:
+      "https://assets.churchofjesuschrist.org/f8/17/f817e4a21336ad73433b4f1f0a5dfca77c6271d41049592/man_studying_reading.jpeg",
+  },
+  {
+    index: 2,
+    heading: "Hello World2",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat.",
+    image:
+      "https://assets.churchofjesuschrist.org/f8/17/f817e4a21336ad73433b4f1f0a5dfca77c6271d41049592/man_studying_reading.jpeg",
+  },
+  {
+    index: 3,
+    heading: "Hello World3",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat.",
+    image:
+      "https://assets.churchofjesuschrist.org/f8/17/f817e4a21336ad73433b4f1f0a5dfca77c6271d41049592/man_studying_reading.jpeg",
+  },
+  {
+    index: 4,
+    heading: "Hello World4",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat.",
+    image:
+      "https://assets.churchofjesuschrist.org/f8/17/f817e4a21336ad73433b4f1f0a5dfca77c6271d41049592/man_studying_reading.jpeg",
+  },
+];
+
+const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-
-  useEffect(() => {
-    // Set the initial active index to the first image
-    setActiveIndex(0);
-  }, [images]);
-
-  const handleClick = (index) => {
-    // Set the active index to the clicked index
-    setActiveIndex(index);
+  let changeTo = 0;
+  const changeSlideHandler = (id) => {
+    setActiveIndex(id);
   };
 
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
-    <div className="carousel">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className={`slide ${activeIndex === index ? "active" : ""}`}
-        >
-          <Card />
+    <div className="">
+      {cards.map((card, index) => (
+        <div>
+          {index === activeIndex && (
+            <Card
+              key={index}
+              active={index === activeIndex}
+              image={card.image}
+              content={card.content}
+              heading={card.heading}
+            ></Card>
+          )}
         </div>
       ))}
-      {controls && (
-        <div className="controls">
-          <button onClick={() => handleClick(activeIndex - 1)}>Previous</button>
-          <button onClick={() => handleClick(activeIndex + 1)}>Next</button>
-        </div>
-      )}
+      <div className="flex justify-center my-5">
+        {cards.map((card, index) => (
+          <a
+            className={classNames(
+              index === activeIndex
+                ? "bg-gray-900"
+                : "bg-gray-200 hover:bg-gray-400 ",
+              "w-3 h-3 mx-1 rounded-full"
+            )}
+            onClick={() => changeSlideHandler(index)}
+          ></a>
+        ))}
+      </div>
     </div>
   );
 };
