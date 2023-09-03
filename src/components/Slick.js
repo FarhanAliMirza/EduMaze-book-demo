1c5cd99163be52c4531687da6178f4c346c71b4c
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2023-09-03 18:49:56 +0530

    slick changes

--- src/components/Slick.js
+++ src/components/Slick.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-
+import Card from "../components/Card";
+//autoplay, pauseonhover,append dots
 export default class Slick extends Component {
   render() {
     const settings = {
@@ -15,10 +16,18 @@ export default class Slick extends Component {
         <h2> Single Item</h2>
         <Slider {...settings}>
           <div>
-            <h3>1</h3>
+            <Card
+              heading="Hello World"
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat."
+              image="https://assets.churchofjesuschrist.org/f8/17/f817e4a21336ad73433b4f1f0a5dfca77c6271d41049592/man_studying_reading.jpeg"
+            />
           </div>
           <div>
-            <h3>2</h3>
+            <Card
+              heading="Hello World"
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat."
+              image="https://assets.churchofjesuschrist.org/f8/17/f817e4a21336ad73433b4f1f0a5dfca77c6271d41049592/man_studying_reading.jpeg"
+            />
           </div>
           <div>
             <h3>3</h3>
