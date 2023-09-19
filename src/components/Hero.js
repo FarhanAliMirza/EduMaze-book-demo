8834bbdfcba5d666c002bb821fc18288b8769458
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2023-09-19 18:21:12 +0530

    Typewritter hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import InputForm from "./InputForm";
 import bg from "../bgimg.png";
 
@@ -10,17 +11,28 @@ const Hero = () => {
           <h1 className="text-white font-bold text-4xl sm:text-6xl sm:leading-relaxed">
             Ut Tellus Elementum Sagittis Vitae
           </h1>
-          <h3 className="text-white mt-3 mr-12 text-lg">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Est
-            placerat in egestas erat.
-          </h3>
+          <div className="text-white mt-3 mr-12 text-lg">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Estplacerat in egestas erat."
+                  )
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Estplacerat in egestas erat"
+                  )
+                  .start();
+              }}
+            />
+          </div>
         </div>
         <div className="justify-center p-6 sm:py-6 mx-3 sm:mr-12 bg-white/20 rounded-3xl">
           <InputForm />
         </div>
       </div>
-      <div className="absolute top-0 overflow-auto-y w-full">
+      <div className="absolute z-[-1] top-0 overflow-auto-y w-full">
         <img
           className="max-w-none sm:w-full opacity-5 sm:opacity-20"
           src={bg}
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -3,9 +3,9 @@ import "../index.css";
 
 const InputForm = (props) => {
   const input =
-    "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-400 appearance-none focus:outline-none focus:ring-0 focus:border-gray-900 peer";
+    "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-light-orange peer";
   const label =
-    "peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-500  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6";
+    "peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-light-orange  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6";
 
   return (
     <form>
@@ -65,7 +65,7 @@ const InputForm = (props) => {
       </div>
       <select
         id="current_class"
-        class="bg-transparent mb-6 text-gray-400 border-gray-400 border-b-2 border-0 text-sm focus:text-gray-900 focus:outline-none focus:ring-0 focus:border-gray-900 block w-full py-2.5 px-0 peer"
+        class="bg-transparent mb-6 text-white border-white border-b-2 border-0 text-sm focus:text-light-orange focus:outline-none focus:ring-0 focus:border-light-orange block w-full py-2.5 px-0 peer"
         required
       >
         <option selected disabled>
@@ -79,7 +79,7 @@ const InputForm = (props) => {
       </select>
       <button
         type="submit"
-        class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+        class="text-white bg-edu-orange hover:bg-light-orange focus:ring-4 focus:outline-none focus:ring-light-orange font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
       >
         Submit
       </button>
